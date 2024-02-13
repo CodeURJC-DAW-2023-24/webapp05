cf631df909af44c7f3a1f09e96544e0105911614
Cherno Valdor <y.labchiri.2021@alumnos.urjc.es>
2024-02-13 10:38:44 +0100

    Some configs for the server

--- src/main/java/es/codeurjc/Instapick/Controller.java
+++ src/main/java/es/codeurjc/Instapick/Controller.java
@@ -1,2 +1,24 @@
-package es.codeurjc.Instapick;public class Controller {
+package es.codeurjc.Instapick;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+    @GetMapping("/login")
+    public String getLogIn(Model model) {
+        return "log_in";
+    }
+
+    @GetMapping("/")
+    public String getMain(Model model) {
+        return "post";
+    }
+
+    @GetMapping("/user")
+    public String getUser(Model model) {
+        return "profile";
+    }
+
 }
