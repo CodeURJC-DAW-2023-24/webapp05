c2215ffb482c3ccb33ac3c53a4bb321143080428
adrianielestudiante <a.aguilarr.2018@alumnos.urjc.es>
2024-02-19 12:21:03 +0100

    Replaced ChatService ChatService
    Replaced ChatService chats type from ChatService to ChatRepository

--- src/main/java/es/codeurjc/Instapick/service/ChatService.java
+++ src/main/java/es/codeurjc/Instapick/service/ChatService.java
@@ -2,6 +2,8 @@ package es.codeurjc.Instapick.service;
 
 import es.codeurjc.Instapick.Model.Chat;
 import es.codeurjc.Instapick.Model.Post;
+import es.codeurjc.Instapick.repository.ChatRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +13,7 @@ import java.util.Optional;
 @Service
 public class ChatService {
 
-    private ChatService chats;
+    private ChatRepository chats;
 
     public void save(Chat chat) {
         chats.save(chat);
